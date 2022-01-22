6f6f4a1e87f58f218df25fd17ae1b9dcbb07862e
Lachlan Miller <lachlan.miller.1990@outlook.com>
2022-01-22 18:19:45 +1000

    add script for creating packages

--- scripts/gulpfile.ts
+++ scripts/gulpfile.ts
@@ -1,4 +1,7 @@
 import gulp from "gulp";
+import fs from "fs-extra";
+import path from "path";
+import inquirer from "inquirer";
 import { spawn } from "child_process";
 
 async function serverDev() {
@@ -21,5 +24,47 @@ async function gameDataServer() {
     cwd: "packages/game-data",
   });
 }
+
+async function createPkg() {
+  console.log(process.env.ar);
+  const results = await inquirer.prompt<{
+    packageName: string;
+  }>([
+    {
+      name: "packageName",
+      type: "input",
+      message: "What is the package name?",
+      validate: (val: string) => /[A-z\-]+/.test(val),
+    },
+  ]);
+
+  const newDir = path.join(__dirname, "..", "packages", results.packageName);
+  await fs.mkdir(newDir);
+
+  await Promise.all([
+    fs.writeFile(
+      path.join(newDir, "package.json"),
+      JSON.stringify(
+        {
+          name: `@packages/${results.packageName}`,
+          version: "0.0.0-development",
+          main: "index.ts",
+          scripts: {
+            test: "echo 'ok'",
+          },
+          dependencies: {},
+          devDependencies: {
+            typescript: "4.5.4",
+          },
+        },
+        null,
+        2
+      )
+    ),
+    fs.writeFile(path.join(newDir, "index.ts"), ""),
+  ]);
+}
+
+gulp.task("createPkg", createPkg);
 
 gulp.task("dev", gulp.series(serverDev, assetsServer, gameDataServer));
