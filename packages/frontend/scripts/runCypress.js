40f066baf9e74586871c5de7f056c0745087aa66
Lachlan Miller <lachlan.miller.1990@outlook.com>
2022-07-20 19:44:13 +1000

    add cypress for production

---
+++ packages/frontend/scripts/runCypress.js
@@ -0,0 +1,17 @@
+const { spawn } = require("child_process");
+
+function run(cmd) {
+  const proc = spawn("yarn", cmd, { shell: true, stdio: "inherit" });
+  proc.on("exit", (code) => process.exit(code || 0));
+  proc.on("error", console.error);
+}
+
+if (process.env.CI) {
+  run(
+    `cypress run --component --record --key 9db0689e-e974-48be-9d9c-5560f615e4be`.split(
+      " "
+    )
+  );
+} else {
+  run(`cypress run --component`.split(" "));
+}
