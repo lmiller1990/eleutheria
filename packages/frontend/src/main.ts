6193cd8d307f36eb792ce54f3b297901c4f0a32d
Lachlan Miller <lachlan.miller.1990@outlook.com>
2022-09-05 23:13:31 +1000

    feat: add signup form

--- packages/frontend/src/main.ts
+++ packages/frontend/src/main.ts
@@ -2,7 +2,7 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import { createPinia } from "pinia";
 import { createRouter } from "./router";
-import "./index.css";
+import "./output.css";
 
 const app = createApp(App);
 app.use(createPinia());
---
+++ packages/frontend/src/validation/index.ts
@@ -0,0 +1,79 @@
+export interface Status {
+  label: string;
+  valid: boolean;
+}
+
+export type ValidatorFn = (actual: string) => Status;
+
+export interface ValidationResult {
+  valid: boolean;
+  validations: Status[];
+}
+
+export function validate(actual: string, validators: ValidatorFn[]) {
+  return validators.reduce<ValidationResult>(
+    (acc, validation) => {
+      const res = validation(actual);
+
+      const next: ValidationResult = {
+        valid: acc.valid === false ? false : res.valid,
+        validations: acc.validations.concat(res),
+      };
+
+      return next;
+    },
+    { validations: [], valid: true }
+  );
+}
+
+export const min = (min: number): ValidatorFn => {
+  return function (actual: string): Status {
+    if (actual.length < min) {
+      return {
+        label: `Min ${min}`,
+        valid: false,
+      };
+    }
+
+    return {
+      label: `Min ${min}`,
+      valid: true,
+    };
+  };
+};
+
+export const max = (max: number): ValidatorFn => {
+  return function (actual: string): Status {
+    if (actual.length > max) {
+      return {
+        label: `Max ${max}`,
+        valid: false,
+      };
+    }
+
+    return {
+      label: `Max ${max}`,
+      valid: true,
+    };
+  };
+};
+
+export const alphanumeric = (): ValidatorFn => {
+  return function (actual: string): Status {
+    const s = actual.replaceAll(/[a-zA-Z0-9_]/g, "");
+
+    if (s.length > 0) {
+      return {
+        label: `Alphanumeric`,
+        valid: false,
+      };
+    }
+
+    return {
+      label: `Alphanumeric`,
+      valid: true,
+    };
+  };
+};
+
+export type Rule = typeof min | typeof max | typeof alphanumeric;
