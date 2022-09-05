b384de9a7fcbf7b511010b8f88a30859127381e4
Lachlan Miller <lachlan.miller.1990@outlook.com>
2022-09-05 15:52:39 +1000

    wip

--- packages/game-data/src/db/index.ts
+++ packages/game-data/src/db/index.ts
@@ -2,6 +2,7 @@ import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = "https://qftkqfrlrbrtnwvczown.supabase.co";
 const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFmdGtxZnJscmJydG53dmN6b3duIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjIzNDcwOTMsImV4cCI6MTk3NzkyMzA5M30.SETCR_SLrMABs2EJdQ0Tl8uQ3nbRjZkouOshaxOPp30"; // process.env.SUPABASE_KEY
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 // console.log(supabase.auth);
