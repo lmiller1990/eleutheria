56d003bbef7e3eebcab4a4305c6806d3c4ba6858
Lachlan Miller <lachlan.miller.1990@outlook.com>
2022-09-26 12:26:30 +1000

    lint

--- packages/frontend/src/screens/summary/SummaryScreen.cy.ts
+++ packages/frontend/src/screens/summary/SummaryScreen.cy.ts
@@ -2,12 +2,16 @@ import SummaryScreen from "./SummaryScreen.vue";
 import { mount } from "../../../cypress/support/mount";
 
 // TODO: Test this.
-describe.skip("SummaryScreen", { viewportHeight: 900, viewportWidth: 1600 }, () => {
-  it("displays score", () => {
-    mount(SummaryScreen, {
-      styles: [
-        "https://fonts.googleapis.com/css2?family=Comfortaa:wght@700&display=swap",
-      ],
+describe.skip(
+  "SummaryScreen",
+  { viewportHeight: 900, viewportWidth: 1600 },
+  () => {
+    it("displays score", () => {
+      mount(SummaryScreen, {
+        styles: [
+          "https://fonts.googleapis.com/css2?family=Comfortaa:wght@700&display=swap",
+        ],
+      });
     });
-  });
-});
+  }
+);
