458dc1ffbdbcc039ad65e286e1ffb7f875c9443c
Lachlan Miller <lachlan.miller.1990@outlook.com>
2022-12-01 22:12:08 +1000

    fix test

--- packages/frontend/src/components/SongTile.cy.tsx
+++ packages/frontend/src/components/SongTile.cy.tsx
@@ -1,6 +1,15 @@
+import { useImageLoader } from "../composables/imageLoader";
 import SongTile from "./SongTile.vue";
 
 describe("SongTile", () => {
+  before(() => {
+    useImageLoader("songSelectScreen", {
+      onAllLoaded: () => {},
+      minimumLoadTimeMs: 0,
+      target: 1,
+    });
+  });
+
   it("renders", () => {
     cy.mount(() => (
       <SongTile
