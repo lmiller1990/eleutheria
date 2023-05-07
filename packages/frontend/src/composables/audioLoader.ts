3bc17674fe25da197321c4621dc74092c1599207
Lachlan Miller <lachlan.miller.1990@outlook.com>
2023-05-07 18:36:31 +1000

    feat: caching (#52)

--- packages/frontend/src/composables/audioLoader.ts
+++ packages/frontend/src/composables/audioLoader.ts
@@ -7,11 +7,32 @@ const wait = async () => new Promise((res) => setTimeout(res, 5));
 
 const throttle = true;
 
+function createDeferred() {
+  return new Promise((resolve) => {
+    window.setTimeout(resolve, 1000);
+  });
+}
+
 async function getAudioData(
   url: string,
   loadingEmitter: LoadingEmitter
 ): Promise<AudioData> {
+  const dfd = createDeferred();
+
   const res = await window.fetch(url);
+  const cache = await caches.open("eleutheria");
+
+  const cachedResponse = await cache.match(url);
+
+  if (cachedResponse) {
+    const buf = await cachedResponse.arrayBuffer();
+    const audioContext = new AudioContext();
+    const audioBuffer = await audioContext.decodeAudioData(buf);
+    loadingEmitter.emit("song:loading:chunk", 100, 100);
+    await dfd;
+
+    return { audioBuffer, audioContext };
+  }
 
   const reader = res.body!.getReader();
   const totalSizeInBytes = parseInt(res.headers.get("content-length")!, 10);
@@ -42,9 +63,12 @@ async function getAudioData(
   });
 
   const response = new Response(stream);
+  await cache.put(url, response.clone());
   const buf = await response.arrayBuffer();
+
   const audioContext = new AudioContext();
   const audioBuffer = await audioContext.decodeAudioData(buf);
+
   return { audioBuffer, audioContext };
 }
 
