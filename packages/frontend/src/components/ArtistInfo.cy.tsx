8060689442949f7afb2949c8648ccdd95c8126e6
Lachlan Miller <lachlan.miller.1990@outlook.com>
2023-06-05 20:29:50 +1000

    work on test

--- packages/frontend/src/components/ArtistInfo.cy.tsx
+++ packages/frontend/src/components/ArtistInfo.cy.tsx
@@ -1,11 +1,26 @@
 import ArtistInfo from "./ArtistInfo.vue";
 
+const composer = {
+  name: "Lachlan",
+  socials: [
+    {
+      social: "twitter",
+      link: "https://twitter.com/Lachlan19900",
+    },
+  ],
+};
+
 describe("<ArtistInfo />", () => {
   it("renders", () => {
     // see: https://on.cypress.io/mounting-vue
     cy.mount(() => (
       <div style="width: 250px">
-        <ArtistInfo artist="Litmus*" />
+        <ArtistInfo
+          artist="Litmus*"
+          composer={composer}
+          stepChart={composer}
+          bannerCreator={composer}
+        />
       </div>
     ));
   });
